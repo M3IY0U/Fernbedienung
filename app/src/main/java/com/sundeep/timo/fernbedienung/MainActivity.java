cd36910ad03a32ff8a6d3fb577b1076683c40b6f
Timo <timo.nock@stud.h-da.de>
2018-12-04 19:23:42 +0100

    Progress on Layout

--- app/src/main/java/com/sundeep/timo/fernbedienung/MainActivity.java
+++ app/src/main/java/com/sundeep/timo/fernbedienung/MainActivity.java
@@ -1,5 +1,7 @@
 package com.sundeep.timo.fernbedienung;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,12 +10,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     SeekBar volumeBar;
     TextView volumeDisplay;
     ImageButton muteButton;
@@ -26,15 +31,16 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         volumeDisplay = (TextView)findViewById(R.id.volumeDisplay);
         muteButton = (ImageButton)findViewById(R.id.muteButton);
-        updateText();
+        updateVolumeText();
         volumeBar = (SeekBar)findViewById(R.id.volumeBar);
+        //volumeBar.setLayoutParams(new LinearLayout.LayoutParams(this.getResources().getDisplayMetrics().widthPixels/2,25));
         volumeBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 Data.getInstance().setVolume(progress);
                 Data.getInstance().setMuted(false);
-                muteButton.setImageResource(R.drawable.baseline_volume_up_black_36);
-                updateText();
+                muteButton.setImageResource(R.drawable.baseline_volume_up_black_48);
+                updateVolumeText();
             }
 
             @Override
@@ -74,14 +80,104 @@ public class MainActivity extends AppCompatActivity {
     public void toggleMute(View v){
         Data.getInstance().toggleMute();
         if(Data.getInstance().isMuted()){
-            muteButton.setImageResource(R.drawable.baseline_volume_off_black_36);
+            muteButton.setImageResource(R.drawable.baseline_volume_off_black_48);
         }else{
-            muteButton.setImageResource(R.drawable.baseline_volume_up_black_36);
+            muteButton.setImageResource(R.drawable.baseline_volume_up_black_48);
         }
-        updateText();
+        updateVolumeText();
     }
 
-    private void updateText(){
+
+    public void startSettings(MenuItem m){
+        Intent i = new Intent(
+                this,
+                SettingsActivity.class);
+        startActivity(i);
+
+    }
+
+    public void startChannelList(View v){
+        Intent i = new Intent(
+                this,
+                ChannelListActivity.class
+        );
+        startActivity(i);
+    }
+
+    public void toggleRatios(View v){
+        LinearLayout ratios = findViewById(R.id.ratioButtons);
+        ImageButton lister = findViewById(R.id.ratioLister);
+
+        if(ratios.getVisibility()==View.GONE){
+            for (int i = 1;i<46;i++){
+                lister.setRotation(i);
+            }
+            expand(ratios,1);
+        }else {
+            for (int i = 45;i>0;i--){
+                lister.setRotation(i);
+            }
+            collapse(ratios,1);
+        }
+    }
+
+
+    private void updateVolumeText(){
         volumeDisplay.setText(Integer.toString(Data.getInstance().getVolume()));
+    }
+
+
+    public static void expand(final View view, int durationMultiplier) {
+        if (view.getVisibility() == View.GONE) {
+
+            view.measure(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            final int targetWidth = view.getMeasuredWidth();
+            view.getLayoutParams().width = 1;
+            view.setVisibility(View.VISIBLE);
+            Animation a = new Animation() {
+
+                @Override
+                protected void applyTransformation(float interpolatedTime, Transformation t) {
+                    view.getLayoutParams().width = (int) (targetWidth * interpolatedTime);
+                    view.requestLayout();
+                }
+
+                @Override
+                public boolean willChangeBounds() {
+                    return true;
+                }
+            };
+
+            a.setDuration(((int) (targetWidth / view.getContext().getResources().getDisplayMetrics().density)) * durationMultiplier);
+            view.startAnimation(a);
+        }
+    }
+
+    public static void collapse(final View view, int durationMultiplier) {
+        if (view.getVisibility() == View.VISIBLE) {
+
+
+            final int initialWidth = view.getMeasuredWidth();
+            Animation a = new Animation() {
+
+                @Override
+                protected void applyTransformation(float interpolatedTime, Transformation t) {
+                    if (interpolatedTime == 1) {
+                        view.setVisibility(View.GONE);
+                    } else {
+                        view.getLayoutParams().width = initialWidth - (int) (initialWidth * interpolatedTime);
+                        view.requestLayout();
+                    }
+                }
+
+                @Override
+                public boolean willChangeBounds() {
+                    return true;
+                }
+            };
+
+            a.setDuration(((int) (initialWidth / view.getContext().getResources().getDisplayMetrics().density)) * durationMultiplier);
+            view.startAnimation(a);
+        }
     }
 }
